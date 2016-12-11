edc97e6ae6674af21e157876622e0df18f9e1950
venster <ashley.vo73@gmail.com>
2016-12-11 10:56:56 -0800

    fixed problems in index.js

--- src/index.js
+++ src/index.js
@@ -3,30 +3,88 @@ var Alexa = require('alexa-sdk');
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 var languageStrings = {
-    "en-US": {
-        "translation": {
-            "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
-            ],
-            "SKILL_NAME" : "American Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
-            "HELP_REPROMPT" : "What can I help you with?",
-            "STOP_MESSAGE" : "Goodbye!"
-        }
-    }
+   "en-US":{
+      "translation":{
+         "JOBS":[
+            {
+               "name":"software engineer",
+               "description":"A software engineer is a person who applies the principles of software engineering to the design, development, maintenance, testing, and evaluation of the software and systems that make computers or anything containing software work.",
+               "percent":"17",
+               "jobLink":"https://collegegrad.com/careers/software-developers",
+               "notable":"Ada Lovelace",
+               "noteDescript":"Ada Lovelace is regarded as the first ever computer programmer.",
+               "personLink":"https://en.wikipedia.org/wiki/Ada_Lovelace"
+            },
+            {
+               "name":"politican",
+               "description":"Politicians are key leaders and decision makers at every level of government. Becoming a politician in the United States allows a person to have a direct effect on many facets of life throughout our cities, states, and country.",
+               "jobLink":"http://www.politicalsciencecareer.com/politician.html",
+               "notable":"Elizabeth Warren",
+               "noteDescript":"She is a Professor from Harvard Law School who easily won the Senate seat of Massachusetts in 2012",
+               "personLink":"https://en.wikipedia.org/wiki/Elizabeth_Warren"
+            },
+            {
+               "name":"surgeon",
+               "description":"A person who is professionally involved in politics, especially as a holder of or a candidate for an elected office.",
+               "percent":"19",
+               "jobLink":"https://collegegrad.com/careers/physicians-and-surgeons",
+               "notable":"Dr. Emily Jennings Stowe",
+               "noteDescript":"When she was told by the University of Canada that the doors never will be open for women, her reply was: Then I will make it the business of my life to see that the doors of Canadian Medical Schools will be opened and that women will have the same opportunities as men.",
+               "personLink":"https://en.wikipedia.org/wiki/Emily_Stowe"
+            },
+            {
+               "name":"civil engineer",
+               "description":"Civil engineers design, build, supervise, operate, and maintain construction projects and systems in the public and private sector, including roads, buildings, airports, tunnels, dams, bridges, and systems for water supply and sewage treatment. Many civil engineers work in design, construction, research, and education.",
+               "percent":"17.5",
+               "jobLink":"https://collegegrad.com/careers/civil-engineers",
+               "notable":"Emily Warren Roebling",
+               "noteDescript":"She oversaw the construction of the Brooklyn Bridge and was considered as the first woman field engineer.",
+               "personLink":"https://en.wikipedia.org/wiki/Emily_Warren_Roebling"
+            },
+            {
+               "name":"movie director",
+               "description":"In the motion picture industry, film directors are in charge of all creative decisions made in the production of a movie, from the beginning stages to the final edits.",
+               "percent":"9",
+               "jobLink":"http://study.com/career_as_a_film_director.html",
+               "notable":"Kathryn Bigelow",
+               "noteDescript":"Bigelow became the first woman to win the Academy Award for Best Director with her film, The Hurt Locker.",
+               "personLink":"https://en.wikipedia.org/wiki/Kathryn_Bigelow"
+            },
+            {
+               "name":"physicist",
+               "description":"A physicist is someone who explores and identifies the basic principles that govern the structure and behaviour of matter, the interaction between energy and matter, and the generation and transfer of energy. These principals can be used in both theoretical and practical areas.",
+               "percent":"20",
+               "jobLink":"http://study.com/physicist_career.html",
+               "notable":"Marie Curie",
+               "noteDescript":"Marie Curie conducted pioneering research on radioactivity and also discovered two elements, polonium and radium.",
+               "personLink":"https://en.wikipedia.org/wiki/Marie_Curie"
+            },
+            {
+               "name":"electrical engineer",
+               "description":"Electrical engineers specialize in power supply and generation. They design, develop, test and supervise electrical equipment manufacturing.",
+               "percent":"10",
+               "jobLink":"http://educatingengineers.com/careers/electrical-engineer",
+               "notable":"Esther M. Conwell",
+               "noteDescript":"Esther M. Conwell studied the properties of semiconductors and organic conductors, which helped lead to the creation of the modern computer",
+               "personLink":"https://en.wikipedia.org/wiki/Esther_M._Conwell"
+            },
+            {
+               "name":"aircraft pilot",
+               "description":"Airline and commercial pilots fly and navigate airplanes, helicopters, and other aircraft. Airline pilots fly for airlines that transport people and cargo on a fixed schedule. Commercial pilots fly aircraft for other purposes, such as charter flights, rescue operations, firefighting, aerial photography, and aerial application, also known as crop dusting.",
+               "percent":"5",
+               "jobLink":"http://www.airaffair.com/Library/start-aviation-career.html",
+               "notable":"Jacqueline Cochran",
+               "noteDescript":"Jacqueline Cochran was a breaker of many flying records and even was the first woman to break the sound barrier.",
+               "personLink":"https://en.wikipedia.org/wiki/Jacqueline_Cochran"
+            }
+         ],
+         "SKILL_NAME":"Women Careers",
+         "GET_RANDOM_MESSAGE":"Here's a random career: ",
+         "HELP_MESSAGE":"You can say name a career, or, you can say exit... What can I help you with?",
+         "HELP_REPROMPT":"What can I help you with?",
+         "STOP_MESSAGE":"Goodbye!"
+      }
+   }
 };
 
 exports.handler = function(event, context, callback) {
@@ -40,21 +98,28 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetFact');
+        this.emit('GetRandom');
     },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
+    'RandomIntent': function () {
+        //this.emit(':tell',"what is up?");
+        this.emit('GetRandom');
     },
-    'GetFact': function () {
+    'GetRandom': function () {
         // Get a random space fact from the space facts list
         // Use this.t() to get corresponding language data
-        var factArr = this.t('FACTS');
-        var factIndex = Math.floor(Math.random() * factArr.length);
-        var randomFact = factArr[factIndex];
+        var jobArr = this.t('JOBS');
+        var jobIndex = Math.floor(Math.random() * jobArr.length);
+        var randomJob = jobArr[jobIndex];
 
-        // Create speech output
-        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact);
+       // Create speech output
+        var speechOutput = this.t("GET_RANDOM_MESSAGE") + randomJob.name+ "\n "+randomJob.description
+                +"\n"+ randomJob.percent +"percent of the industry is composed of women. A notable female " 
+                            +randomJob.name+ " is "
+                            +randomJob.notable +". \n"
+                            +randomJob.noteDescript;
+        var cardOutput =  randomJob.description+ " only " + randomJob.percent+ "% are female.";
+        //this.emit(':tell',speechOutput);
+        this.emit(':tellWithCard', speechOutput, randomJob.name, cardOutput);
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = this.t("HELP_MESSAGE");
