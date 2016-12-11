a9fe9490ed74d5b835b043f02c6f8eef7d5be0e0
Ken Luo <luozhirong@gmail.com>
2016-12-10 20:19:24 -0800

    added intent json

---
+++ src/intent.js
@@ -0,0 +1,25 @@
+{
+    "intents": [
+	{
+	    "intent": "GetAstronautInformationIntent"
+	},
+	{
+	    "intent": "GetSoftwareEngineerInformationIntent"
+	},
+	{
+	    "intent": "GetLawyerInformationIntent"
+	},
+	{
+	    "intent": "GetBusinessInformationIntent"
+	},
+	{
+	    "intent": "AMAZON.HelpIntent"
+	},
+	{
+	    "intent": "AMAZON.StopIntent"
+	},
+	{
+	    "intent": "AMAZON.CancelIntent"
+	}
+    ]
+}
